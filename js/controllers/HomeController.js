5d1dbfabc7e315a381527e152d24330ee3f15f73
Monty <monty@macbook.metaltoad.net>
2015-10-10 13:00:42 -0700

    succesfully load images.

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -2,14 +2,29 @@ app.controller('HomeController',
   ['$scope', function($scope) {
     $scope.cartoons = [
       { title: 'cartoon5',
-        image: 'cartoons/VFD_0005.jpg'},
+        image: 'cartoons/VFD_0005.jpg',
+        alt:'',
+        number:'0005'
+      },
       { title: 'cartoon4',
-        image: 'cartoons/VFD_0004.jpg'},
+        image: 'cartoons/VFD_0004.jpg',
+        alt:'',
+        number:'0004'
+      },
       { title: 'cartoon3',
-        image: 'cartoons/VFD_0003.jpg'},
+        image: 'cartoons/VFD_0003.jpg',
+        alt:'',
+        number:'0003'
+      },
       { title: 'cartoon2',
-        image: 'cartoons/VFD_0002.jpg'},
+        image: 'cartoons/VFD_0002.jpg',
+        alt:'',
+        number:'0002'
+      },
       { title: 'cartoon1',
-        image: 'cartoons/VFD_0001.jpg'}
+        image: 'cartoons/VFD_0001.jpg',
+        alt:'',
+        number:'0001'
+      }
     ];
   }]);
