9613192c43fff99563c26cf3f893c02b651d6913
Monty <monty@Macbook.SilverStarTelecom.net>
2015-12-17 20:38:59 -0800

    Loads a new image everytime a button is clicked.

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -8,13 +8,22 @@
 
   function HomeController($scope, $http, imageLoaderService) {
 
+    $scope.totalDisplayed = 0;
     $scope.loadMore = loadMore;
 
-    function loadMore(){
+    function loadMore() {
       imageLoaderService.getPromise().then(function(data){
-        $scope.cartoons = data.data.cartoons;
+          $scope.cartoons = data.data.cartoons;
       });
+      $scope.totalDisplayed += 1;
     }
+
+    // function loadMore(){
+    //     var last = $scope.cartoons[$scope.cartoons.length - 1];
+    //       for(var i = 1; i <= 190; i++) {
+    //         $scope.cartoons.push(last + i);
+    //       }
+    // }
      // $scope.images = [1, 2, 3, 4, 5, 6, 7, 8];
 
   // $scope.loadMore = function() {
